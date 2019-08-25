936b4ee550c7f9e24dd7084c3d0312701799849a
Pifagor <zheka.rachkovan@gmail.com>
2019-08-25 21:14:07 +0300

    server template

--- server.go
+++ server.go
@@ -6,8 +6,14 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
-	})
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/chat", chatHandler)
+	http.HandleFunc("/quest", questHandler)
+	http.HandleFunc("/", notFoundHandler)
 	http.ListenAndServe(":80", nil)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "404 Not Found")
 }
